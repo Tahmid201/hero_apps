ee611b2dbae8fa3464e2ac21da7b2b46ad240fa9
Tahmid201 <abdusshafitahmid635@gmail.com>
2025-10-18 22:11:25 +0600

    modified

--- src/Pages/ErrorPages/ErrorPages.jsx
+++ src/Pages/ErrorPages/ErrorPages.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import heroImage from "../../assets/error404.png"
+import heroImage from "../../assets/error404.png";
+
 export default function ErrorPage() {
   return (
-    <div className="text-center mt-20">
-      <img src={heroImage} alt="" />
-      <Link to="/" className="text-blue-600 underline">
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+      <img
+        className="w-80 max-w-full mb-6"
+        src={heroImage}
+        alt="404 Error"
+      />
+      <Link
+        to="/"
+        className="text-blue-600 underline text-lg hover:text-blue-800 transition"
+      >
         Go Home
       </Link>
     </div>
