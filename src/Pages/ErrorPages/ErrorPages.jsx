ac197cd9945ce212764253daeb52577cea66d341
Tahmid201 <abdusshafitahmid635@gmail.com>
2025-10-18 21:57:19 +0600

    second commit

--- src/Pages/ErrorPages/ErrorPages.jsx
+++ src/Pages/ErrorPages/ErrorPages.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-
-const ErrorPages = () => {
-    return (
-        <div>
-            <h1>hey there, i am error page</h1>
-        </div>
-    );
-};
-
-export default ErrorPages;
+import React from "react";
+import { Link } from "react-router-dom";
+import heroImage from "../../assets/error404.png"
+export default function ErrorPage() {
+  return (
+    <div className="text-center mt-20">
+      <img src={heroImage} alt="" />
+      <Link to="/" className="text-blue-600 underline">
+        Go Home
+      </Link>
+    </div>
+  );
+}
